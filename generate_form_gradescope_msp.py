5dc3c9fca1bf2a0c72ff8fe6c5b49e17d3aed88d
Nathan Sprague <Nathan Sprague>
2022-02-01 14:01:43 -0500

    add late penalties for gradescope msp

--- generate_form_gradescope_msp.py
+++ generate_form_gradescope_msp.py
@@ -1,9 +1,9 @@
 #! /usr/bin/env python
-"""Generate an XML form from Canvas and Autolab csv files that can be
+"""Generate an XML form from Canvas and Gradescope csv files that can be
 uploaded to Canvas.
 
 This script can be used when a single Canvas grade should be the sum
-of multiple Autolab grades.
+of multiple Gradescope grades.
 
 Author: Nathan Sprague + Phil Riley
 Version: 1/27/2022
@@ -13,9 +13,21 @@ import argparse
 import generate_form
 import re
 import os
+import math
 
-def autolab_assignment_name(filename):
-    """Extract the autolab assessment name for an autolab csv grade
+LATE_SLACK = .01 # about 14 minutes
+
+def days_late(late_str):
+    if len(late_str) == 0:
+        return 0
+
+    days, hours, mins = (int(s) for s in late_str.split(":"))
+    late_time_in_days = days + hours / 24 + mins / 1440 - LATE_SLACK
+    return int(math.floor(late_time_in_days + 1))
+
+
+def gradescope_assignment_name(filename):
+    """Extract the gradescope assessment name for an gradescope csv grade
     file name.
 
     """
@@ -27,28 +39,30 @@ def autolab_assignment_name(filename):
 
 class GradescopeForm(generate_form.CanvasForm):
 
-    def __init__(self, canvas_csv, assignment_id, autolab_csvs):
+    def __init__(self, canvas_csv, assignment_id, gradescope_csvs,
+                 late_penalty):
 
         super(GradescopeForm, self).__init__(canvas_csv, assignment_id, pad=True)
 
         self.student_dicts = [] # one dictionary per exercise/csv file
         self.table_headers = []
         self.ex_names = []
-        for autolab_csv in autolab_csvs:
-            self.ex_names.append(autolab_assignment_name(autolab_csv))
+        self.late_penalty = late_penalty
+        for gradescope_csv in gradescope_csvs:
+            self.ex_names.append(gradescope_assignment_name(gradescope_csv))
 
-            autolab_table = generate_form.csv_to_numpy(autolab_csv, pad=True)
+            gradescope_table = generate_form.csv_to_numpy(gradescope_csv, pad=True)
 
             cur_headers = {}
-            for col in range(autolab_table.shape[1]):
-                cur_headers[autolab_table[0, col].strip(':')] = col
+            for col in range(gradescope_table.shape[1]):
+                cur_headers[gradescope_table[0, col].strip(':')] = col
             self.table_headers.append(cur_headers)
 
             cur_entries = {}
             email_col = cur_headers['Email']
-            for i in range(1, autolab_table.shape[0], 1):
-                eid = autolab_table[i, email_col].split('@')[0]
-                cur_entries[eid] = list(autolab_table[i, :])
+            for i in range(1, gradescope_table.shape[0], 1):
+                eid = gradescope_table[i, email_col].split('@')[0]
+                cur_entries[eid] = list(gradescope_table[i, :])
             self.student_dicts.append(cur_entries)
 
 
@@ -58,21 +72,33 @@ class GradescopeForm(generate_form.CanvasForm):
         comments = ""
         tool_score = 0
         try:
-            table_data = zip(self.student_dicts, self.ex_names, self.table_headers)
+            table_data = zip(self.student_dicts, self.ex_names,
+                             self.table_headers)
             for student_data, ex_name, table_headers in table_data:
                 cur_score = student_data[eid][table_headers['Total Score']]
+                
+                late_str = student_data[eid][table_headers['Lateness (H:M:S)']]
+                late = days_late(late_str)
+                    
                 comments += ex_name + ": "
                 if cur_score == "not_submitted" or \
                    cur_score == "not_yet_submitted" or \
                    cur_score == '':
-                    comments += "No Autolab submission.\n"
+                    comments += "No Gradescope submission.\n"
                     cur_score = "0"
+                elif late > 0 and self.late_penalty > 0:
+                    multiplier = round(max(0, 1 - self.late_penalty * late), 2)
+                    score = round(float(cur_score) * multiplier, 2)
+                    comments += "{} x {} = {} (late)\n".format(cur_score,
+                                                               multiplier,
+                                                               score)
+                    cur_score = score
                 else:
                     comments += cur_score + "\n"
 
                 tool_score += float(cur_score)
         except KeyError as e:
-            # This may happen if a student is in Canvas, but not autolab
+            # This may happen if a student is in Canvas, but not gradescope
             pass
         
         return comments, tool_score
@@ -81,8 +107,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("canvas_csv", metavar="CANVAS_CSV")
     parser.add_argument("canvas_assignment_id",  metavar="CANVAS_ASSIGNMENT_ID")
-    parser.add_argument("autolab_csv", metavar="AUTOLAB_CSV", nargs='*')
+    parser.add_argument("-l", "--late-penalty", type=float,
+                        help="Late penalty per day (default .15)", default=.15)
+    parser.add_argument("gradescope_csv", metavar="GRADESCOPE_CSV", nargs='*')
     args = parser.parse_args()
     wf = GradescopeForm(args.canvas_csv, args.canvas_assignment_id,
-                     args.autolab_csv)                    
+                        args.gradescope_csv, args.late_penalty)
     wf.generate_form()
